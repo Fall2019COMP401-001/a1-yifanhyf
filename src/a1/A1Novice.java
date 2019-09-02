23ef5b3815ba8f06f47d12a4c3c4642eaf9ddc7d
yifan <yifan@172.16.200.108>
2019-09-01 21:58:00 -0400

    Commented out code that printed version.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,42 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int numberOfCustomer = scan.nextInt();
+		
+		for (int i=0; i<numberOfCustomer; i++) {
+			
+		// Total number of customers
+		
+		String firstName = scan.next();
+		String lastName = scan.next();
+		// Name of the customer
+		
+		int numberOfItem = scan.nextInt();
+		// Items of this customer bought
+		
+		double priceTotal = 0;
+		// If the customer bought nothing, the price would be 0
+		
+		String priceAtEnd = "";
+		
+		for (int j=0; j<numberOfItem; j++) {
+		
+		int quantity = scan.nextInt();
+		String foodName = scan.next();
+		double priceSingle = scan.nextDouble();
+		// All items' quantity, name and their price
+		
+		priceTotal = priceSingle * quantity;
+		priceAtEnd = String.format("%.2f", priceTotal);
+		}
+		// Total price for each customer
+		
+		char firstInitial = firstName.charAt(0);
+		
+		System.out.println(firstInitial + ". " + lastName + ": " + priceAtEnd);
+
+		
+		}
 	}
 }
